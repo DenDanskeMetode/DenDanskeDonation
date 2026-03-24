a5bde632b9f7cc7b0859ce8aefdd949b9bd9a45e
vilo0001 <vilo0001@stud.kea.dk>
2026-03-24 10:58:28 +0100

    Dansk standard tal: "1.000kr"

--- frontend/src/pages/CampaignDetail.js
+++ frontend/src/pages/CampaignDetail.js
@@ -117,7 +117,7 @@ function CampaignDetail() {
               donations.map(d => (
                 <div key={d.id} className="cd-donation-entry">
                   <div className="cd-donation-who">{d.sender_firstname ?? d.sender_username}</div>
-                  <div className="cd-donation-amount">{d.amount} kr</div>
+                  <div className="cd-donation-amount">{Number(d.amount).toLocaleString('da-DK')} kr</div>
                   <div className="cd-donation-when">{timeAgo(d.created_at)}</div>
                 </div>
               ))
